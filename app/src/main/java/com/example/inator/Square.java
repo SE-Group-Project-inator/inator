1809feef5d7e6931ce634c26cad2af57b9a517ee
umer-khalid1 <umerkhalid00423@gmail.com>
2026-02-16 10:44:20 +0500

    added square class extending shape

---
+++ app/src/main/java/com/example/inator/Square.java
@@ -0,0 +1,11 @@
+package com.example.inator;
+
+public class Square extends Shape{
+    double length;
+
+    Square(int x, int y, double length) {
+        this.x = x;
+        this.y = y;
+        this.length = length;
+    }
+}
