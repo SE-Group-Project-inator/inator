4a7a68624cfb5d8c7d4e9c1923c5f5ccca6c95c4
Salsabeel <27100339@lums.edu.pk>
2026-02-16 14:19:53 +0500

    Added Star Class' Attribute and Methods

--- app/src/main/java/com/example/inator/Star.java
+++ app/src/main/java/com/example/inator/Star.java
@@ -1,5 +1,17 @@
 package com.example.inator;
 
 public class Star extends Shape{
+    private int vertices;
 
+    public Star(int vertices) {
+        this.vertices = vertices;
+    }
+
+    public int getVertices() {
+        return vertices;
+    }
+
+    public void setVertices(int vertices) {
+        this.vertices = vertices;
+    }
 }
